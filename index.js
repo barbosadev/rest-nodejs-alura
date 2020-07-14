5527c476e636cefaff8bc6cd984b464e95bc0ed4
Victor Barbosa <vicldb99@gmail.com>
2020-07-14 19:05:58 -0300

    Instalação, primeira rota com express e nodemon

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+});
+
+app.get('/atendimentos', (req, res) => {
+    res.send('Atendimentos');
+});
